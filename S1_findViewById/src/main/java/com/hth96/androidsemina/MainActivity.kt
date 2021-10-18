982d4f27a0e729d927a00f650e28fa320a4cf0b7
hunglm <hunglm@runsystem.net>
2021-10-18 15:18:18 +0700

    - First commit

---
+++ S1_findViewById/src/main/java/com/hth96/androidsemina/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.hth96.androidsemina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var value = 10
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tvValue = findViewById<AppCompatTextView>(R.id.tv_value)
+        val btnPlus = findViewById<AppCompatButton>(R.id.btn_plus)
+        val btnMinus = findViewById<AppCompatButton>(R.id.btn_minus)
+
+        tvValue.text = "Your value is $value"
+        btnPlus.setOnClickListener {
+            value++
+            tvValue.text = "Your value is $value"
+        }
+        btnMinus.setOnClickListener {
+            value--
+            tvValue.text = "Your value is $value"
+        }
+    }
+}
