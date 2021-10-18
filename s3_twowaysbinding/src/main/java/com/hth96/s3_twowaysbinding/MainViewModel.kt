d175878be3fe67ad997e065fb56a29c40445e910
hunglm <hunglm@runsystem.net>
2021-10-18 17:18:46 +0700

    - Two ways binding

--- s3_twowaysbinding/src/main/java/com/hth96/s3_twowaysbinding/MainViewModel.kt
+++ s3_twowaysbinding/src/main/java/com/hth96/s3_twowaysbinding/MainViewModel.kt
@@ -5,11 +5,16 @@ import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    val name = MutableLiveData("")
+    val yourName = Transformations.map(name) {
+        "Your name is $it"
+    }
+
     private val value = MutableLiveData(10)
     val text = Transformations.map(value) {
         "Your value is $it"
     }
-
 
     fun minus() {
         value.postValue(value.value as Int - 1)
