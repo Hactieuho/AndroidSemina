ad1f6bc157a4df015c971c02cfb17f81178f72bc
hunglm <hunglm@runsystem.net>
2021-10-18 15:23:04 +0700

    - Databinding

---
+++ s2_databinding/src/main/java/com/hth96/s2_databinding/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.hth96.s2_databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.hth96.s2_databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private var value = 10
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.tvValue.text = "Your value is $value"
+        binding.btnPlus.setOnClickListener {
+            value++
+            binding.tvValue.text = "Your value is $value"
+        }
+        binding.btnMinus.setOnClickListener {
+            value--
+            binding.tvValue.text = "Your value is $value"
+        }
+    }
+}
