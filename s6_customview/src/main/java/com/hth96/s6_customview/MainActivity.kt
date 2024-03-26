ce9f06d1b05be8580d7ab1ff1ab0de1e425caecc
hunglm <hunglm@its-global.vn>
2024-03-26 10:38:42 +0700

    Add customview

---
+++ s6_customview/src/main/java/com/hth96/s6_customview/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.hth96.s6_customview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.hth96.s6_customview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+    }
+}
---
+++ s6_customview/src/main/java/com/hth96/s6_customview/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.hth96.s6_customview
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
