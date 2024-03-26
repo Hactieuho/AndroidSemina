0f10efe236bcb705a761f48ce7a16d95db0743a5
hunglm <hunglm@its-global.vn>
2024-03-26 16:49:48 +0700

    Add attribute

--- s6_customview/src/main/java/com/hth96/s6_customview/EmotionalFaceView.kt
+++ s6_customview/src/main/java/com/hth96/s6_customview/EmotionalFaceView.kt
@@ -10,21 +10,65 @@ import android.util.AttributeSet
 import android.view.View
 
 class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    // Paint object for coloring and styling
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    // 1
+    companion object {
+        private const val DEFAULT_FACE_COLOR = Color.YELLOW
+        private const val DEFAULT_EYES_COLOR = Color.BLACK
+        private const val DEFAULT_MOUTH_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_WIDTH = 4.0f
 
-    // Some colors for the face background, eyes and mouth.
-    private var faceColor = Color.YELLOW
-    private var eyesColor = Color.BLACK
-    private var mouthColor = Color.BLACK
-    private var borderColor = Color.BLACK
+        const val HAPPY = 0L
+        const val SAD = 1L
+    }
 
-    // Face border width in pixels
-    private var borderWidth = 4.0f
+    // 2
+    private var faceColor = DEFAULT_FACE_COLOR
+    private var eyesColor = DEFAULT_EYES_COLOR
+    private var mouthColor = DEFAULT_MOUTH_COLOR
+    private var borderColor = DEFAULT_BORDER_COLOR
+    private var borderWidth = DEFAULT_BORDER_WIDTH
 
-    // View size in pixels
-    private var size = 320
+    private val paint = Paint()
     private val mouthPath = Path()
+    private var size = 0
+
+    // 3
+    var happinessState = HAPPY
+        set(state) {
+            field = state
+            // 4
+            invalidate()
+        }
+
+    // 5
+    init {
+        paint.isAntiAlias = true
+        setupAttributes(attrs)
+    }
+
+    private fun setupAttributes(attrs: AttributeSet?) {
+        // 6
+        // Obtain a typed array of attributes
+        val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.EmotionalFaceView,
+            0, 0)
+
+        // 7
+        // Extract custom attributes into member variables
+        happinessState = typedArray.getInt(R.styleable.EmotionalFaceView_state, HAPPY.toInt()).toLong()
+        faceColor = typedArray.getColor(R.styleable.EmotionalFaceView_faceColor, DEFAULT_FACE_COLOR)
+        eyesColor = typedArray.getColor(R.styleable.EmotionalFaceView_eyesColor, DEFAULT_EYES_COLOR)
+        mouthColor = typedArray.getColor(R.styleable.EmotionalFaceView_mouthColor, DEFAULT_MOUTH_COLOR)
+        borderColor = typedArray.getColor(R.styleable.EmotionalFaceView_borderColor,
+            DEFAULT_BORDER_COLOR)
+        borderWidth = typedArray.getDimension(R.styleable.EmotionalFaceView_borderWidth,
+            DEFAULT_BORDER_WIDTH)
+
+        // 8
+        // TypedArray objects are shared and must be recycled.
+        typedArray.recycle()
+    }
+
 
     override fun onDraw(canvas: Canvas) {
         // call the super method to keep any drawing from the parent side.
@@ -91,4 +135,14 @@ class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, a
         canvas.drawPath(mouthPath, paint)
 
     }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+// 1
+        size = Math.min(measuredWidth, measuredHeight)
+// 2
+        setMeasuredDimension(size, size)
+
+    }
+
 }
