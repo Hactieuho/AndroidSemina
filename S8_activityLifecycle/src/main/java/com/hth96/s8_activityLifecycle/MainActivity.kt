6e1f21edb594a720aaff4193efb74b20b0d847d1
hunglm <hunglm@its-global.vn>
2024-04-16 16:30:11 +0700

    Add lifecycle state

--- S8_activityLifecycle/src/main/java/com/hth96/s8_activityLifecycle/MainActivity.kt
+++ S8_activityLifecycle/src/main/java/com/hth96/s8_activityLifecycle/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
-        viewModel.addText("1. onCreate")
+        viewModel.addText("1. onCreate, ${lifecycle.currentState}")
 
         setOnClickListener()
         observeLiveData()
@@ -49,61 +49,61 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        viewModel.addText("2. onStart")
+        viewModel.addText("2. onStart, ${lifecycle.currentState}")
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        viewModel.addText("2.1. onRestoreInstanceState")
+        viewModel.addText("2.1. onRestoreInstanceState, ${lifecycle.currentState}")
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
-        viewModel.addText("3. onPostCreate")
+        viewModel.addText("3. onPostCreate, ${lifecycle.currentState}")
     }
 
     override fun onResume() {
         super.onResume()
-        viewModel.addText("4. onResume")
+        viewModel.addText("4. onResume, ${lifecycle.currentState}")
     }
 
     override fun onPostResume() {
         super.onPostResume()
-        viewModel.addText("4.1 onPostResume")
+        viewModel.addText("4.1 onPostResume, ${lifecycle.currentState}")
     }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        viewModel.addText("4.2. onAttachedToWindow")
+        viewModel.addText("4.2. onAttachedToWindow, ${lifecycle.currentState}")
     }
 
     override fun onPause() {
         super.onPause()
-        viewModel.addText("5. onPause")
+        viewModel.addText("5. onPause, ${lifecycle.currentState}")
     }
 
     override fun onStop() {
         super.onStop()
-        viewModel.addText("6. onStop")
+        viewModel.addText("6. onStop, ${lifecycle.currentState}")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        viewModel.addText("6.1. onSaveInstanceState")
+        viewModel.addText("6.1. onSaveInstanceState, ${lifecycle.currentState}")
     }
 
     override fun onRestart() {
         super.onRestart()
-        viewModel.addText("6.2. onRestart")
+        viewModel.addText("6.2. onRestart, ${lifecycle.currentState}")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        viewModel.addText("7. onDestroy")
+        viewModel.addText("7. onDestroy, ${lifecycle.currentState}")
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        viewModel.addText("7.1. onDetachedFromWindow")
+        viewModel.addText("7.1. onDetachedFromWindow, ${lifecycle.currentState}")
     }
 }
