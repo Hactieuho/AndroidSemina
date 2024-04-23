9e067e2fa4df17a8736e26ecc051f198e0472644
hunglm <hunglm@its-global.vn>
2024-04-23 14:14:01 +0700

    Add fragments lifecycle

--- S9_fragmentLifecycle/src/main/java/com/hth96/s9_fragmentLifeCycle/Fragment1.kt
+++ S9_fragmentLifecycle/src/main/java/com/hth96/s9_fragmentLifeCycle/Fragment1.kt
@@ -1,9 +1,7 @@
 package com.hth96.s9_fragmentLifeCycle
 
 import android.content.Context
-import android.content.res.Configuration
 import android.os.Bundle
-import android.util.AttributeSet
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,19 +15,19 @@ class Fragment1 : Fragment() {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        viewModel.addText("1. onAttach, ${lifecycle.currentState}")
+        viewModel.addText("F1. onAttach, ${lifecycle.currentState}")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.addText("2. onCreate, ${lifecycle.currentState}")
+        viewModel.addText("F2. onCreate, ${lifecycle.currentState}")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewModel.addText("3. onCreateView, ${lifecycle.currentState}")
+        viewModel.addText("F3. onCreateView, ${lifecycle.currentState}")
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment1, container, false)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
@@ -38,52 +36,52 @@ class Fragment1 : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.addText("4. onViewCreated, ${lifecycle.currentState}")
+        viewModel.addText("F4. onViewCreated, ${lifecycle.currentState}")
     }
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
         super.onViewStateRestored(savedInstanceState)
-        viewModel.addText("4.1. onViewStateRestored, ${lifecycle.currentState}")
+        viewModel.addText("F4.1. onViewStateRestored, ${lifecycle.currentState}")
     }
 
     override fun onStart() {
         super.onStart()
-        viewModel.addText("5. onStart, ${lifecycle.currentState}")
+        viewModel.addText("F5. onStart, ${lifecycle.currentState}")
     }
 
     override fun onResume() {
         super.onResume()
-        viewModel.addText("6. onResume, ${lifecycle.currentState}")
+        viewModel.addText("F6. onResume, ${lifecycle.currentState}")
     }
 
     override fun onPause() {
         super.onPause()
-        viewModel.addText("7. onPause, ${lifecycle.currentState}")
+        viewModel.addText("F7. onPause, ${lifecycle.currentState}")
     }
 
     override fun onStop() {
         super.onStop()
-        viewModel.addText("8. onStop, ${lifecycle.currentState}")
+        viewModel.addText("F8. onStop, ${lifecycle.currentState}")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        viewModel.addText("8.1. onSaveInstanceState, ${lifecycle.currentState}")
+        viewModel.addText("F8.1. onSaveInstanceState, ${lifecycle.currentState}")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        viewModel.addText("9. onDestroyView, ${lifecycle.currentState}")
+        viewModel.addText("F9. onDestroyView, ${lifecycle.currentState}")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        viewModel.addText("10. onDestroy, ${lifecycle.currentState}")
+        viewModel.addText("F10. onDestroy, ${lifecycle.currentState}")
     }
 
     override fun onDetach() {
         super.onDetach()
-        viewModel.addText("11. onDetach, ${lifecycle.currentState}")
+        viewModel.addText("F11. onDetach, ${lifecycle.currentState}")
     }
 
     companion object {
